145c99fb52329ce349d0859146f196a88976a264
Michal Vlasák <michal.vlasak@ackee.cz>
2019-11-05 11:09:27 +0100

    🔥 Remove pino pretty
    Replaced by util.inspect

--- src/streams.ts
+++ src/streams.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as pino from 'pino';
 import { Transform, TransformCallback } from 'stream';
+import * as util from 'util';
 import { AckeeLoggerOptions, AckeeLoggerStream } from './interfaces';
 import { levels } from './levels';
 import { StackDriverFormatStream } from './stackdriver';
@@ -16,20 +17,23 @@ const getDefaultTransformStream = (options: AckeeLoggerOptions & { messageKey: s
         public _transform(chunk: any, _encoding: string, callback: TransformCallback) {
             const obj = JSON.parse(chunk);
             const loggerName = options.loggerName;
+            let res;
             if (options.pretty) {
-                obj['name\0'] = obj.name; // add null character so that it is not interpreted by pino-pretty but still visible to user unchanged
+                // obj['name\0'] = obj.name; // add null character so that it is not interpreted by pino-pretty but still visible to user unchanged
                 delete obj.name;
                 if (loggerName) {
                     obj.name = loggerName;
                 }
+                res = util.inspect(obj, { colors: true, showHidden: true, depth: 10 });
             } else {
                 obj.pkgVersion = pkgJson.version;
                 if (obj[options.messageKey] && isString(obj[options.messageKey]) && loggerName) {
                     obj[options.messageKey] = `[${loggerName}] ${obj[options.messageKey]}`;
                 }
+                res = JSON.stringify(obj);
             }
 
-            this.push(`${JSON.stringify(obj)}\n`);
+            this.push(`${res}\n`);
             callback();
         }
     }
@@ -55,35 +59,17 @@ const initLoggerStreams = (
     let streams: AckeeLoggerStream[];
     if (options.streams) {
         streams = options.streams.map(stream => Object.assign({ level: defaultLevel }, stream));
-    } else if (options.pretty) {
-        const pretty = pino({
-            prettyPrint: {
-                levelFirst: true,
-            },
-        });
-        pretty.pipe(process.stdout);
-        const prettyErr = pino({
-            prettyPrint: {
-                levelFirst: true,
-            },
-        });
-        prettyErr.pipe(process.stderr);
-        streams = [
-            { level: defaultLevel, maxLevel: levels.warn, stream: pretty as any },
-            { level: 'warn', stream: prettyErr as any },
-        ];
     } else {
         streams = [
             { level: defaultLevel, maxLevel: levels.warn, stream: process.stdout },
             { level: 'warn', stream: process.stderr },
         ];
     }
-    if (!options.disableStackdriverFormat) {
+    if (!options.pretty && !options.disableStackdriverFormat) {
         streams = decorateStreams(streams, StackDriverFormatStream);
     }
 
     streams = decorateStreams(streams, getDefaultTransformStream(options));
-
     return streams;
 };
 
