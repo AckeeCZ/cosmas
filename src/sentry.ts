da6e5dcbb6ca5671b2a3de1bd40a564b9aa1677e
Jaroslav Šmolík <grissius@gmail.com>
2019-11-25 08:15:03 +0100

    ♻️ Refactor sentry levels
    Related: #28

--- src/sentry.ts
+++ src/sentry.ts
@@ -12,17 +12,18 @@ const reportToSentry = (obj: any) => {
     return captureException(error);
 };
 
+const PINO_TO_SENTRY: { [key: number]: Severity } = {
+    10: Severity.Debug,
+    20: Severity.Debug,
+    30: Severity.Info,
+    40: Severity.Warning,
+    50: Severity.Error,
+    60: Severity.Critical,
+};
+
 class SentryTransformStream extends Transform {
     // tslint:disable-next-line:function-name
     public _transform(chunk: any, _encoding: string, callback: TransformCallback) {
-        const PINO_TO_SENTRY: { [key: number]: Severity } = {
-            10: Severity.Debug,
-            20: Severity.Debug,
-            30: Severity.Info,
-            40: Severity.Warning,
-            50: Severity.Error,
-            60: Severity.Critical,
-        };
         const obj = JSON.parse(chunk);
         withScope(scope => {
             scope.setLevel(PINO_TO_SENTRY[obj.level]);
